296b2f2952d4ca3f66b4bbb969e115a26a2b0e38
Gamatek <mathieu.bontemps3.0@gmail.com>
2026-06-12 09:11:53 +0200

    refactor(constants): removal of PacketType and GamePacket (already moved to NetworkManager)

--- Pong/Constants.h
+++ Pong/Constants.h
@@ -19,21 +19,3 @@ const enum GameMode {
     MODE_NET_SEARCH = 4,
     MODE_SETTINGS = 5
 };
-
-const enum PacketType {
-    PACKET_TYPE_DISCOVERY_REQUEST,
-    PACKET_TYPE_DISCOVERY_RESPONSE,
-    PACKET_TYPE_PLAYER_JOIN,
-    PACKET_TYPE_PADDLE_UPDATE,
-    PACKET_TYPE_BALL_UPDATE,
-    PACKET_TYPE_SCORE_UPDATE,
-    PACKET_TYPE_GAME_START
-};
-
-const struct GamePacket {
-    PacketType type;
-    int playerId; // 1 or 2
-    double x, y;
-    double vx, vy;
-    int score1, score2;
-};
